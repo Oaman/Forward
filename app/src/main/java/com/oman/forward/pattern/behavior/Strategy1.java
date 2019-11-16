c1d19510d2c033b6611ee8c1f2736a3092f030b1
ZHOU Jiang <jizhou.ext@orange.com>
2019-11-16 15:36:49 +0800

    add strategy pattern

---
+++ app/src/main/java/com/oman/forward/pattern/behavior/Strategy1.java
@@ -0,0 +1,17 @@
+package com.oman.forward.pattern.behavior;
+
+public class Strategy1 implements Strategy {
+
+    StringBuilder builder = new StringBuilder("select * from user where ");
+
+    @Override
+    public String getSQL(String[] names) {
+        if (names != null && names.length != 0) {
+            for (String s : names) {
+                builder.append("name = ").append(s).append(" or ");
+            }
+        }
+        builder.delete(builder.length() - 4, builder.length());
+        return builder.toString();
+    }
+}
---
+++ app/src/main/java/com/oman/forward/pattern/behavior/Strategy2.java
@@ -0,0 +1,25 @@
+package com.oman.forward.pattern.behavior;
+
+public class Strategy2 implements Strategy {
+
+    StringBuilder builder = new StringBuilder("select * from user where ");
+
+    @Override
+    public String getSQL(String[] names) {
+        boolean needOr = false;
+        /**
+         * 此处使用boolean控制是否插入or字符
+         * 将or放到最前面 先不添加or 添加一个之后将boolean值设置为true就可以添加or了
+         * 或者将
+         */
+        for (String s : names) {
+            if (needOr) {
+                builder.append(" or ");
+            }
+            builder.append("name = ");
+            builder.append(s);
+            needOr = true;
+        }
+        return builder.toString();
+    }
+}
